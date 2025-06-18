7b676901c3c232777d64b7e242f469222de4de3d
kazirshahria <kazirshahria@gmail.com>
2025-06-17 22:05:07 -0400

    Functions shared across all instances

---
+++ src/bot/utils/bot_utils.py
@@ -0,0 +1,11 @@
+import requests
+
+def json_response(url: str):
+    response = requests.request(
+        method='GET',
+        url=url
+    )
+
+    if response.status_code == 200:
+        return response.json()
+    
