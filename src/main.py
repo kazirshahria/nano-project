18b4c5b651bc42b56fbfee7c6b52a967b0fec214
kazirshahria <kazirshahria@gmail.com>
2025-06-20 15:11:03 -0400

    Testing instances

--- src/main.py
+++ src/main.py
@@ -1,5 +1,22 @@
-from bot.prizepicks import PrizePicks
+# from bot.prizepicks import PrizePicks
+# from bot.underdog import UnderDog
+from bot.database import Database
 
-prizepicks = PrizePicks()
-props = prizepicks.current_props()
-print(props)
+# prizepicks = PrizePicks()
+# underdog = UnderDog()
+
+# props = prizepicks.current_props()
+# print(props)
+
+
+# props = underdog.current_props()
+# print(props)
+
+try:
+    db = Database()
+    data = db.table('prizepicks_lines')
+    print(data)
+except Exception as e:
+    print(e)
+finally:
+    db.close_connection()
