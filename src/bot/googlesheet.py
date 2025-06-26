bd8d8d26a4ab4c903940a5c75fbb340f75c594b0
kazirshahria <kazirshahria@gmail.com>
2025-06-26 18:16:17 -0400

    Setup to detect Google Sheet workbook

--- src/bot/googlesheet.py
+++ src/bot/googlesheet.py
@@ -1,1 +1,16 @@
+import os
+from gspread import service_account
+import pandas as pd
 
+class GoogleSheet(object):
+    
+    def __init__(self, sheet_url: str = os.environ['GSHEET_URL']):
+        service_account_login = service_account(filename='google_credentials.json')
+        self.client = service_account_login.open_by_url(sheet_url)
+    
+    def worksheet_instance(self, id: str = '635725027'):
+        return self.client.get_worksheet_by_id(id=id)
+    
+    def update_worksheet(self, worksheet, df: pd.DataFrame):
+        df = df.astype(str)
+        return worksheet.update([df.columns.values.tolist()] + df.values.tolist(), value_input_option="USER_ENTERED")
